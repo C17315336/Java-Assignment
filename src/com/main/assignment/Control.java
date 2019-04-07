121cd5dd3859cebb448f250bc2ab34acfb9fa992
“Eoghan <C17315336@mydit.ie>
2019-04-07 21:44:24 +0100

    Empty project

---
+++ src/com/main/assignment/Control.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.main.assignment;
+
+/**
+ * @author Eoghan
+ *
+ */
+public class Control {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
