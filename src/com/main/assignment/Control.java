0a2cc8f9ee3b350da7ef99fa70ca8fef9b6d7a2d
“Eoghan <C17315336@mydit.ie>
2019-04-08 00:37:57 +0100

    File import sample

---
+++ src/com/main/assignment/Control.java
@@ -0,0 +1,190 @@
+package com.main.assignment;
+
+import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import javax.swing.table.DefaultTableModel;
+
+public class Control extends JFrame {
+	
+	private JTable table;
+
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				Control form = new Control();
+				form.setVisible(true);
+			}
+		});
+	}
+
+	public Control() {
+
+		// Create Form Frame
+		super("ThaiCreate.Com Java GUI Tutorial");
+		setSize(668, 345);
+		setLocation(500, 280);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		getContentPane().setLayout(null);
+
+		// Label Result
+		final JLabel lblResult = new JLabel("Result", JLabel.CENTER);
+		lblResult.setBounds(150, 22, 370, 14);
+		getContentPane().add(lblResult);
+
+		// Table
+		table = new JTable();
+		getContentPane().add(table);
+		
+		// Table Model
+		final DefaultTableModel model = (DefaultTableModel)table.getModel();
+		model.addColumn("CustomerID");
+		model.addColumn("Name");
+		model.addColumn("Email");
+		model.addColumn("CountryCode");
+		model.addColumn("Budget");
+		model.addColumn("Used");
+		
+		// ScrollPane
+		JScrollPane scroll = new JScrollPane(table);
+		scroll.setBounds(84, 98, 506, 79);
+		getContentPane().add(scroll);
+
+
+		// Create Button Open JFileChooser
+		JButton btnButton = new JButton("Open File Chooser");
+		btnButton.setBounds(268, 47, 135, 23);
+		btnButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+
+				JFileChooser fileopen = new JFileChooser();
+				FileFilter filter = new FileNameExtensionFilter(
+						"Text/CSV file", "txt", "csv");
+				fileopen.addChoosableFileFilter(filter);
+
+				int ret = fileopen.showDialog(null, "Choose file");
+
+				if (ret == JFileChooser.APPROVE_OPTION) {
+
+					// Read Text file
+					File file = fileopen.getSelectedFile();
+
+					try {
+						BufferedReader br = new BufferedReader(new FileReader(
+								file));
+						String line;
+						int row = 0;
+						while ((line = br.readLine()) != null) {
+							String[] arr = line.split(",");
+							model.addRow(new Object[0]);
+							model.setValueAt(arr[0], row, 0);
+							model.setValueAt(arr[1], row, 1);
+							model.setValueAt(arr[2], row, 2);
+							model.setValueAt(arr[3], row, 3);
+							model.setValueAt(arr[4], row, 4);
+							model.setValueAt(arr[5], row, 5);
+							row++;
+						}
+						br.close();
+					} catch (IOException ex) {
+						// TODO Auto-generated catch block
+						ex.printStackTrace();
+					}
+
+					lblResult.setText(fileopen.getSelectedFile().toString());
+				}
+
+			}
+		});
+		getContentPane().add(btnButton);
+		
+		// Button Save
+		JButton btnSave = new JButton("Save");
+		btnSave.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				SaveData(); // save Data
+			}
+		});
+		btnSave.setBounds(292, 228, 89, 23);
+		getContentPane().add(btnSave);
+
+	}
+	
+	
+	private void SaveData()
+	{
+		Connection connect = null;
+		Statement s = null;
+
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+
+			connect = DriverManager.getConnection(""
+					+ "jdbc:mysql://localhost/mydatabase"
+					+ "?user=root&password=root");
+
+			s = connect.createStatement();
+			
+			for(int i = 0; i<table.getRowCount();i++)
+			{
+				String CustomerID = table.getValueAt(i, 0).toString();
+				String Name = table.getValueAt(i, 1).toString();
+				String Email = table.getValueAt(i, 2).toString();
+				String CountryCode = table.getValueAt(i, 3).toString();
+				String Budget = table.getValueAt(i, 4).toString();
+				String Used = table.getValueAt(i, 5).toString();
+				
+				// SQL Insert
+
+				String sql = "INSERT INTO customer "
+						+ "(CustomerID,Name,Email,CountryCode,Budget,Used) "
+						+ "VALUES ('" + CustomerID + "','"
+						+ Name + "','"
+						+ Email + "'" + ",'"
+						+ CountryCode + "','"
+						+ Budget + "','"
+						+ Used + "') ";
+				s.execute(sql);
+			}
+				
+			JOptionPane.showMessageDialog(null,
+					"Import Data Successfully");
+
+
+		} catch (Exception ex) {
+			// TODO Auto-generated catch block
+			JOptionPane.showMessageDialog(null, ex.getMessage());
+			ex.printStackTrace();
+		}
+
+		try {
+			if (s != null) {
+				s.close();
+				connect.close();
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			System.out.println(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+}
