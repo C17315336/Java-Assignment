81874b4eb518315d342bb06670183d9415d27d74
“Eoghan <C17315336@mydit.ie>
2019-04-11 07:55:22 +0100

    SQL to build table

---
+++ table.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `assignment` (
+  `StopNumber` int(6) NOT NULL,
+  `NamewithoutLocality` varchar(100) NOT NULL,
+  `Locality` varchar(100) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Easting` int(7) NOT NULL,
+  `Northing` int(7) NOT NULL
+);
