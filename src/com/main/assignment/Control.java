2d55ff1d645c5cec5f6b8e1e374655e4ef8a0f66
“Eoghan <C17315336@mydit.ie>
2019-04-12 17:02:45 +0100

    Cleaned and updated - Control

--- src/com/main/assignment/Control.java
+++ src/com/main/assignment/Control.java
@@ -1,5 +1,19 @@
 package com.main.assignment;
 
+/**
+ * Control Class for Java Assignment
+ * 	Class used to launch program
+ * 
+ * Compiled on the 12th of April 2019
+ * By: 	Eoghan Byrne
+ * 		eoghan.byrne4@mydit.ie
+ * 
+ * Using JavaSE 1.8
+ * with references libs of;
+ * 		- MySQL Connector
+ * 		- DbUtils
+ */
+
 import javax.swing.JFrame;
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
