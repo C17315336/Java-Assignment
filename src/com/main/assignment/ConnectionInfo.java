599b761ab1d57890db614e941480f5a1839eeee4
“Eoghan <C17315336@mydit.ie>
2019-04-12 17:51:24 +0100

    Cleaned and updated - ConnectionInfo

--- src/com/main/assignment/ConnectionInfo.java
+++ src/com/main/assignment/ConnectionInfo.java
@@ -1,6 +1,23 @@
 package com.main.assignment;
 
+/**
+ * ConnectionInfo Class for Java Assignment
+ * 	Class used to store database connection information
+ *
+ * Compiled on the 12th of April 2019
+ * By: 	Eoghan Byrne
+ * 		eoghan.byrne4@mydit.ie
+ *
+ * Using JavaSE 1.8
+ * with references libs of;
+ * 		- MySQL Connector
+ * 		- DbUtils
+ */
+
 public class ConnectionInfo {
+	/**
+	 * Attributes - with default connection values
+	 */
 	private static String dbdomain = "localhost";
 	private static String dbport = "8889";
 	private static String dbuser = "Eoghan";
@@ -9,8 +26,10 @@ public class ConnectionInfo {
 	private static String dbtable = "assignment";
 	private static String dburl = "jdbc:mysql://" + dbdomain + ":" + dbport + "/" + dbdata;
 	private static int header = 0;
-	
-	
+
+	/**
+	 * Setters and Getters
+	 */
 	public static String getDbdomain() {
 		return dbdomain;
 	}
@@ -59,6 +78,4 @@ public class ConnectionInfo {
 	public static void setHeader(int header) {
 		ConnectionInfo.header = header;
 	}
-	
-	
 }
