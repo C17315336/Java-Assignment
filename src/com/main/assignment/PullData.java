525ee4e2366edcca5bd5791a1a4230cbc04beca1
“Eoghan <C17315336@mydit.ie>
2019-04-12 17:58:23 +0100

    Cleaned and updated - PullData

--- src/com/main/assignment/PullData.java
+++ src/com/main/assignment/PullData.java
@@ -1,5 +1,19 @@
 package com.main.assignment;
 
+/**
+ * PullData Class for Java Assignment
+ * 	Class used to pull data from database
+ *
+ * Compiled on the 12th of April 2019
+ * By: 	Eoghan Byrne
+ * 		eoghan.byrne4@mydit.ie
+ *
+ * Using JavaSE 1.8
+ * with references libs of;
+ * 		- MySQL Connector
+ * 		- DbUtils
+ */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -13,12 +27,12 @@ import net.proteanit.sql.DbUtils;
 public class PullData {
 	public PullData(JTable tbTable) {
 		Connection connect = null;
-		Statement s = null;
+		Statement state = null;
 
 		try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
 			connect = DriverManager.getConnection(ConnectionInfo.getDburl(), ConnectionInfo.getDbuser(), ConnectionInfo.getDbpass());
-			s = connect.createStatement();
+			state = connect.createStatement();
 			PreparedStatement st = connect.prepareStatement("Select * from " + ConnectionInfo.getDbtable() + ";");
 			ResultSet rs = st.executeQuery();
 			tbTable.setModel(DbUtils.resultSetToTableModel(rs));
