4b5d03df313e878e2c246282b37e58dc49cd615f
“Eoghan <C17315336@mydit.ie>
2019-04-12 17:55:48 +0100

    Cleaned and updated - Filter

--- src/com/main/assignment/Filter.java
+++ src/com/main/assignment/Filter.java
@@ -1,5 +1,19 @@
 package com.main.assignment;
 
+/**
+ * Filter Class for Java Assignment
+ * 	Class used to filter table contents
+ *
+ * Compiled on the 12th of April 2019
+ * By: 	Eoghan Byrne
+ * 		eoghan.byrne4@mydit.ie
+ *
+ * Using JavaSE 1.8
+ * with references libs of;
+ * 		- MySQL Connector
+ * 		- DbUtils
+ */
+
 import javax.swing.JTable;
 import javax.swing.RowFilter;
 import javax.swing.table.TableModel;
@@ -9,14 +23,13 @@ public class Filter {
 	public Filter(JTable tbTable, String text) {
 		TableRowSorter<TableModel> sorter = new TableRowSorter<>(tbTable.getModel());
 		tbTable.setRowSorter(sorter);
-		
+
 		if (text.length() == 0) {
 			sorter.setRowFilter(null);
 		} else {
 			sorter.setRowFilter(RowFilter.regexFilter(text));
 		}
-		
+
 		sorter.sort();
 	}
-
 }
