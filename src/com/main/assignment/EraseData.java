c27d160ea7f182e3be24571e9cbf334f0e9f2e2d
“Eoghan <C17315336@mydit.ie>
2019-04-12 17:54:36 +0100

    Cleaned and updated - EraseData

--- src/com/main/assignment/EraseData.java
+++ src/com/main/assignment/EraseData.java
@@ -1,5 +1,19 @@
 package com.main.assignment;
 
+/**
+ * EraseData Class for Java Assignment
+ * 	Class used to erase database contents
+ *
+ * Compiled on the 12th of April 2019
+ * By: 	Eoghan Byrne
+ * 		eoghan.byrne4@mydit.ie
+ *
+ * Using JavaSE 1.8
+ * with references libs of;
+ * 		- MySQL Connector
+ * 		- DbUtils
+ */
+
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -10,23 +24,20 @@ import javax.swing.JOptionPane;
 public class EraseData {
 	public EraseData() {
 		Connection connect = null;
-		Statement s = null;
+		Statement state = null;
 
 		try {
+			// Connecting to database
 			Class.forName("com.mysql.cj.jdbc.Driver");
-
 			connect = DriverManager.getConnection(ConnectionInfo.getDburl(), ConnectionInfo.getDbuser(), ConnectionInfo.getDbpass());
-
-			s = connect.createStatement();
+			state = connect.createStatement();
 
 				// SQL Insert
-
 				String sql = "DELETE FROM `" + ConnectionInfo.getDbtable() + "` WHERE `StopNumber` > 0";
-				s.execute(sql);
-			
-
+				state.execute(sql);
+				
+		// Feedback to user
 		JOptionPane.showMessageDialog(null, "Data Erased Successfully");
-
 	} catch (Exception ex) {
 		// TODO Auto-generated catch block
 		JOptionPane.showMessageDialog(null, ex.getMessage());
